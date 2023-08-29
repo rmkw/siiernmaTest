af425465cd19d949fe4f8356570d8633429457a7
rmkw <creditozona1@gmail.com>
2023-08-29 13:37:04 -0600

    se modifico la parte movil de la primer pagina para una mejor comprencion :fire:

--- src/app/dggma/pages/layout-page/layout-page.component.ts
+++ src/app/dggma/pages/layout-page/layout-page.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./layout-page.component.css']
 })
 export class LayoutPageComponent {
+  getLogoClass(): string {
+  return window.innerWidth <= 768 ? 'logo-mobile' : 'logo-desktop';
+}
+
 
 }
