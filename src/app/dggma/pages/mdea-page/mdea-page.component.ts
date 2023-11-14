e755716810e9670381129f76d295b68dd633a238
rmkw <creditozona1@gmail.com>
2023-11-14 14:35:28 -0600

    fix MDEA and ODS

--- src/app/dggma/pages/mdea-page/mdea-page.component.ts
+++ src/app/dggma/pages/mdea-page/mdea-page.component.ts
@@ -1,9 +1,4 @@
-import { Products } from './../../interfaces/product.interface';
-import { Componente, Mdea, Subcomponente, Topico } from '../../interfaces/mdea.interface';
 import { Component, OnInit } from '@angular/core';
-import { DGService } from '../../services/dg.service';
-import { TreeNode } from 'primeng/api';
-
 
 @Component({
   selector: 'app-mdea-page',
@@ -13,140 +8,11 @@ import { TreeNode } from 'primeng/api';
 
 export class MdeaPageComponent  implements OnInit{
 
-    treeDataMdea: TreeNode[] = [];
-    selectedNodesMdea: any;
+  constructor(
+    ){}
 
-    public products: Products[] = [];
-    public componentesMDEA: Componente[]=[];
-    public subComponentesMDEA: Subcomponente[]=[];
-    public topicoMDEA: Topico[]=[];
-    public mdeas: Mdea[]=[] ;
-
-    subcomponentes: Subcomponente[] =[];
-    topicos: Topico[] = [];
-
-    filteredProducts: Products[] = [];
-    combinedResultsMdea: Mdea[]=[] ;
-
-
-
-    constructor(
-      private _direServices: DGService,
-
-    ){this.combinedResultsMdea = [];}
-
-
-    filterProductsBy(): void {
-      this.filteredProducts = this.products.filter(data => this.combinedResultsMdea.some(mdeas => mdeas.interview__id === data.interview__id));
-      console.log(this.filteredProducts);
-    }
-
-
-
-
-  selectComponente(event: { originalEvent: Event, node: TreeNode }): void {
-    const key = event.node.key;
-    if (key) {
-      const keyParts = key.split('_');
-      const tipo = keyParts[0];
-      const id = keyParts[1];
-      let componentResults: Mdea[] = [];
-      if (tipo === 'componente') {
-        componentResults = this.mdeas.filter((mdeas) => mdeas.comp_mdea === +id);
-      } else if (tipo === 'subcomponente') {
-        componentResults = this.mdeas.filter((mdeas) => mdeas.subcomp_mdea === +id);
-      } else if (tipo === 'topico') {
-        componentResults = this.mdeas.filter((mdeas) => mdeas.topico_mdea === +id);
-      }
-      this.combinedResultsMdea = this.combinedResultsMdea.concat(componentResults);
-      this.filterProductsBy()
-    }
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
   }
 
-
-
-
-
-
-  UnSelectComponente(event: { originalEvent: Event, node: TreeNode }): void {
-    const key = event.node.key;
-    if (key) {
-      const keyParts = key.split('_');
-      const tipo = keyParts[0];
-      const id = keyParts[1];
-      if (!this.combinedResultsMdea) {
-        this.combinedResultsMdea = [];
-      }
-      this.combinedResultsMdea = this.combinedResultsMdea.filter((mdea: any) => {
-        if (tipo === 'componente') {
-          return mdea.comp_mdea !== +id;
-        } else if (tipo === 'subcomponente') {
-          return mdea.subcomp_mdea !== +id;
-        } else if (tipo === 'topico') {
-          return mdea.topico_mdea !== +id;
-        }
-        return true;
-      });
-      console.log(this.combinedResultsMdea)
-      console.log(this.filteredProducts);
-      this.filterProductsBy()
-  }
-}
-
-
-
-
-
-    ngOnInit(): void {
-
-      // Funcion que manda a llamar el servicio y los datos de este para que se pueda combinar con la transformaciión de datos a la estructura de treenode
-      this._direServices.componentes().subscribe(componentes => {
-        this._direServices.subcomponentes().subscribe(subcomponentes => {
-          this._direServices.topicos().subscribe(topicos => {
-            this.treeDataMdea = this.transformDataToTreeNodeMdea(componentes, subcomponentes, topicos);
-          });
-        });
-      });
-
-
-      this._direServices.productos()
-      .subscribe(data => this.products = data )
-
-      this._direServices.mdea()
-      .subscribe(data => this.mdeas = data)
-
-    }
-
-    //Funcion que ayuda a transformar los arreglos de objetos de componentes, subcomponentes y topicos para que este pueda ser compatible con la estructura treenode
-    transformDataToTreeNodeMdea(componentes: Componente[], subcomponentes: Subcomponente[], topicos: Topico[]): TreeNode[] {
-
-      return componentes.map(componente => {
-        const subcomponenteNodes: TreeNode[] = subcomponentes
-          .filter(subcomponente => subcomponente.parentid === componente.id)
-          .map(subcomponente => {
-            const topicoNodes: TreeNode[] = topicos
-              .filter(topico => topico.parentid === subcomponente.id)
-              .map(topico => ({
-                key: `topico_${topico.id}`,
-                label: topico.text,
-                data: topico,
-
-              }));
-            return {
-              key: `subcomponente_${subcomponente.id}`,
-              label: subcomponente.text,
-              data: subcomponente,
-              children: topicoNodes,
-
-            };
-          });
-        return {
-          key: `componente_${componente.id}`,
-          label: componente.text,
-          data: componente,
-          children: subcomponenteNodes,
-
-        };
-      });
-  }
 }
--- src/app/dggma/pages/ods-page/ods-page.component.ts
+++ src/app/dggma/pages/ods-page/ods-page.component.ts
@@ -1,8 +1,4 @@
-import { MetaODS, SecuenciaOds, Ods } from './../../interfaces/ods.interface';
-import { DGService } from './../../services/dg.service';
 import { Component, OnInit } from '@angular/core';
-import { Products } from '../../interfaces/product.interface';
-import { TreeNode } from 'primeng/api';
 
 @Component({
   selector: 'app-ods-page',
@@ -11,100 +7,10 @@ import { TreeNode } from 'primeng/api';
 })
 export class OdsPageComponent implements OnInit{
 
-  treeDataOds: TreeNode[] = [];
-  selectedNodesOds: any;
-  public products: Products[] = [];
-  public ODSes: SecuenciaOds[]=[];
-  public objetivODS: Ods[]=[];
-  public metasODS: MetaODS[]=[];
-  objetivo: Ods[] = [];
-  meta: MetaODS []= [];
+  constructor(){}
 
-  filteredProducts: Products[] = [];
-
-  constructor(
-    private _direServices: DGService
-    )
-    {}
-
-
-    scrollToTop() {
-      window.scrollTo({ top: 0, behavior: 'smooth' });
-    }
-
-
-  odsByProducts(){
-
-      this.filteredProducts = this.products.filter(data => this.ODSes.some(ODSes => ODSes.interview__id === data.interview__id))
-       console.log(this.filteredProducts)
-   }
-
-
-  //  Función para mandar a llamar el filtrado y seleccion de los checkboxes dentro del HTML
-
-  onNodeSelectOds(event: { originalEvent: Event, node: TreeNode }, nodeType: string): void {
-    const id = event.node.data.id; // Obtén el id del nodo seleccionado
-
-    switch (nodeType) {
-      case 'objetivo':
-        this._direServices.odsByObjetivo(id).subscribe(data => {
-          this.ODSes = data;
-          this.odsByProducts()
-          console.log(id);
-        });
-        break;
-      case 'meta':
-        this._direServices.odsByMeta(id).subscribe(data => {
-          this.ODSes = data;
-          this.odsByProducts()
-          console.log(id);
-        });
-        break;
-    }
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
   }
-
-  onNodeUnselectOds(event: { originalEvent: Event, node: TreeNode }, nodeType: string): void {
-    const id = event.node.data.id;
-    this.filteredProducts = [];
-
-  }
-
-
-  ngOnInit(): void{
-
-
-  this._direServices.objetivos().subscribe(objetivoOds => {
-    this._direServices.metas().subscribe(metasODS =>  {
-        this.treeDataOds = this.transformDataToTreeNodeOds(objetivoOds, metasODS);
-      });
-    });
-
-
-  this._direServices.productos()
-  .subscribe(data => this.products = data )
-
-  }
-
-  transformDataToTreeNodeOds(objetivoOds: Ods[], metaOds: MetaODS[]): TreeNode[] {
-    return objetivoOds.map(objetivo => {
-      const objetivosNodes: TreeNode[] = metaOds
-        .filter(metas => metas.parentid === objetivo.id)
-        .map(metas => {
-          return {
-            key: `metas_${metas.id}`,
-            label: metas.text,
-            data: metas,
-            selected: false
-          };
-        });
-      return {
-        key: `objetivos_${objetivo.id}`,
-        label: objetivo.text.slice(3, 50),
-        data: objetivo,
-        children: objetivosNodes,
-        selected: false
-      };
-    });
- }
 
 }
