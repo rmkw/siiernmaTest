9f26052ffae62a0c216ec54439e06a2316732663
rmkw <creditozona1@gmail.com>
2024-08-08 10:24:59 -0600

    hot fix no funciono el script para seleccion de base de datos

--- src/app/environments/environments.ts
+++ src/app/environments/environments.ts
@@ -1,4 +1,4 @@
 export const environments = {
-  production: false,
-  baseUrl: 'http://localhost:8080',
-};
+  // baseUrl: 'http://localhost:8080'
+  baseUrl: 'https://siiernmadata.onrender.com'
+}
