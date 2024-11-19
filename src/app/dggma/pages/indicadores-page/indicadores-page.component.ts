9161a02289b4d91ffcc78b861be91ff73657b520
Sergio Almanza <sergioeddy1@gmail.com>
2024-11-19 13:15:15 -0600

    deploy test v2, cambios preliminares

--- src/app/dggma/pages/indicadores-page/indicadores-page.component.ts
+++ src/app/dggma/pages/indicadores-page/indicadores-page.component.ts
@@ -10,6 +10,10 @@ import { IndicadorTable } from '../../interfaces/indicador.interface';
 })
 export class IndicadoresPageComponent implements OnInit {
   indicadores: IndicadorTable[] = [];
+  paginatedData: any[] = []; // Datos mostrados en la tabla
+  isLoading: boolean = true; // Estado de carga
+  first: number = 0; // Índice del primer registro visible
+  rows: number = 15; // Número de registros por página
 
   constructor(public indi_table: DGService) {
     this.indi_table.indicadoresCollectionTable().subscribe(
@@ -23,16 +27,37 @@ export class IndicadoresPageComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.cargarIndicadores();
+  }
+
+  cargarIndicadores(): void {
+    this.isLoading = true; 
     this.indi_table.indicadoresCollectionTable().subscribe(
       (data: IndicadorTable[]) => {
         this.indicadores = data;
-        this.filteredIndicadores = [...this.indicadores]; // Inicializa con todos los indicadores
+        this.filteredIndicadores = [...this.indicadores]; 
+        this.updatePaginatedData();
+        this.isLoading = false; 
       },
       (error) => {
         console.error('Error al obtener los indicadores', error);
+        this.isLoading = false; 
       }
     );
   }
+
+  updatePaginatedData(): void {
+    const start = this.first;
+    const end = this.first + this.rows;
+    this.paginatedData = this.filteredIndicadores.slice(start, end);
+  }
+
+  onPageChange(event: any): void {
+    this.first = event.first;
+    this.rows = event.rows;
+    this.updatePaginatedData();
+  }
+
 
   filteredIndicadores: IndicadorTable[] = [];
 
@@ -42,7 +67,7 @@ export class IndicadoresPageComponent implements OnInit {
         (indicador) => indicador.tema === tema
       );
     } else {
-      this.filteredIndicadores = [...this.indicadores]; // Muestra todos los indicadores
+      this.filteredIndicadores = [...this.indicadores]; 
     }
   }
 }
